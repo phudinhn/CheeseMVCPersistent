3ade5e31af2872cff795bf6becb13b28da5472ad
phudinhn <phudinhn@gmail.com>
2018-06-15 23:20:17 -0500

    Add Category controller.

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using CheeseMVC.Data;
+using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController (CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<CheeseCategory> category = context.Categories.ToList();
+            return View(category);
+        }
+    }
+}
