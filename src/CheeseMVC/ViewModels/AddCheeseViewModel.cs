cdc3a4bbb7b08ddedc0dcab3cc7df47f6f57654d
phudinhn <phudinhn@gmail.com>
2018-06-18 16:08:00 -0500

    Update AddCheese view model to reflect model update.

--- src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
+++ src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace CheeseMVC.ViewModels
 {
@@ -16,32 +17,31 @@ namespace CheeseMVC.ViewModels
 
         [Required(ErrorMessage = "You must give your cheese a description")]
         public string Description { get; set; }
+        
+        [Required]
+        [Display(Name="Category")]
+        public int CategoryID { get; set; }
 
-        public CheeseType Type { get; set; }
+        public List<SelectListItem> Categories { get; set; }
 
-        public List<SelectListItem> CheeseTypes { get; set; }
+        public AddCheeseViewModel ()
+        {
 
-        public AddCheeseViewModel() {
+        }
 
-            CheeseTypes = new List<SelectListItem>();
+        public AddCheeseViewModel(IEnumerable<CheeseCategory> categories) {
+
+            Categories = new List<SelectListItem>();
 
             // <option value="0">Hard</option>
-            CheeseTypes.Add(new SelectListItem {
-                Value = ((int) CheeseType.Hard).ToString(),
-                Text = CheeseType.Hard.ToString()
-            });
-
-            CheeseTypes.Add(new SelectListItem
+            foreach (var category in categories)
             {
-                Value = ((int)CheeseType.Soft).ToString(),
-                Text = CheeseType.Soft.ToString()
-            });
-
-            CheeseTypes.Add(new SelectListItem
-            {
-                Value = ((int)CheeseType.Fake).ToString(),
-                Text = CheeseType.Fake.ToString()
-            });
+                Categories.Add(new SelectListItem
+                {
+                    Value = category.ID.ToString(),
+                    Text = category.Name.ToString(),
+                });
+            }
 
         }
     }
