9bf7dae59a647065909bb0c0a7e241bd9902cbf1
phudinhn <phudinhn@gmail.com>
2018-06-20 07:57:49 -0500

    ViewModels for CheeseMenus and Menu models.

---
+++ src/CheeseMVC/ViewModels/AddMenuViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using CheesePersistence.Models;
+
+namespace CheesePersistence.ViewModels
+{
+    public class AddMenuViewModel
+    {
+        public AddMenuViewModel()
+        {
+        }
+
+        public AddMenuViewModel(List<Menu> list)
+        {
+        }
+
+        [Required]
+        [Display(Name = "Menu Name")]
+        public string Name { get; set; }
+    }
+}
---
+++ src/CheeseMVC/ViewModels/ViewMenuViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CheesePersistence.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheesePersistence.ViewModels
+{
+    public class ViewMenuViewModel
+    {
+        public IList<CheeseMenu> Items { get; set; }
+        public Menu Menu { get; set; }
+    }
+}
